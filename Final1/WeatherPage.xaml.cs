4809bc9f01ae99395bf2195cb55bc7af3ed0aed2
Eduardo Martinez <marti540@couagrs.csusm.edu>
2019-04-21 14:54:18 -0700

    updated weather

--- Final1/WeatherPage.xaml.cs
+++ Final1/WeatherPage.xaml.cs
@@ -32,17 +32,19 @@ namespace Final1
                 System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
                 dateTime = dateTime.AddSeconds(timestamp);
                 weather.Daily.Data[i].Day = dateTime;
-
+                weather.Daily.Data[i].Icon = weather.Daily.Data[i].Icon + ".json";
                 //weather.Daily.Data[i].Day = new DateTime(weather.Daily.Data[i].Time);
             }
+    
             WeeklyWeatherListview.ItemsSource = weather.Daily.Data;
             SetCurrent(weather);
         }
 
         private void SetCurrent(Weather weather)
         {
+            currentanimationView.Animation = weather.Currently.Icon + ".json";
             Condtion.Text = weather.Currently.Summary.ToString();
-            Temperature.Text = weather.Currently.Temperature.ToString();
+            Temperature.Text = weather.Currently.Temperature.ToString() + "°";
             humidty.Text = weather.Currently.Humidity.ToString();
             Wind.Text = weather.Currently.WindSpeed.ToString();
             UV.Text = weather.Currently.UvIndex.ToString();
