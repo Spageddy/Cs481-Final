9b885b2edbfa5818d500354796f5ba4fe5575a69
Eduardo Martinez <marti540@couagrs.csusm.edu>
2019-04-23 18:18:02 -0700

    added food page

--- Final1/YelpPage.xaml.cs
+++ Final1/YelpPage.xaml.cs
@@ -10,6 +10,26 @@ namespace Final1
         public YelpPage()
         {
             InitializeComponent();
+            SearchYelp();
+        }
+
+        async void SearchYelp()
+        {
+            string apiKey = "nkM6iyU8QrPv-_ExXt7_bDRVT8yxSEmvPXBMs7d9zO8f8ZrFmervQr9dI3jBVTLBQrnfG9x6yiCsL40ebj9D7Slr7KnothJLWPCvUDO4m_P_ZZQZ7i-errcNguyyXHYx";
+            var client = new Yelp.Api.Client(apiKey);
+            var results = await client.SearchBusinessesAllAsync("cupcakes", 37.786882, -122.399972);
+
+            YelpList.ItemsSource = results.Businesses;
+        }
+
+        async void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+           
+
+            var selected =(Yelp.Api.Models.BusinessResponse)e.Item;
+            string fixedurl = selected.Url.ToString();
+            await Navigation.PushAsync(new WebPage(fixedurl), true);
+
         }
     }
 }
